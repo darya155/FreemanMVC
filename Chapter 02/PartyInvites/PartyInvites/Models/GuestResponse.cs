e0dac0c39eb434dfcfba2f50b670e93be4bff219
darya155 <fiola@mail.ru>
2017-03-29 10:38:55 +0300

    Валидация. Подключение стилей. Отправка email участникам.

--- Chapter 02/PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ Chapter 02/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter your email address")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Please enter valid email adress")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please enter your phone number")]
         public string Phone { get; set; }
-        public bool WillAttend { get; set; }
+
+        [Required(ErrorMessage ="Please specify whether you`ll attend")]
+        public bool? WillAttend { get; set; }//Тип bool? имеет три возможных значения: true, false и null
     }
 }
